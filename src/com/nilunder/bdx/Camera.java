a519a33dbe1fa46b4cfabd20a6fc81661a415166
SolarLune <solarlune@gmail.com>
2015-02-07 08:19:29 -0800

    screenPosition() function to convert world coordinates to screen coordinates (#92).

--- src/com/nilunder/bdx/Camera.java
+++ src/com/nilunder/bdx/Camera.java
@@ -1,5 +1,8 @@
 package com.nilunder.bdx;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector3;
+
 import javax.vecmath.*;
 
 public class Camera extends GameObject{
@@ -9,4 +12,13 @@ public class Camera extends GameObject{
 		m.set(scene.cam.projection.getValues());
 		return m;
 	}
+
+	public Vector2f screenPosition(Vector3f worldPosition){
+
+		Vector3 out = scene.cam.project(new Vector3(worldPosition.x, worldPosition.y, worldPosition.z));
+
+		return new Vector2f(out.x / Gdx.app.getGraphics().getWidth(), out.y / Gdx.app.getGraphics().getHeight());
+
+	}
+
 }
